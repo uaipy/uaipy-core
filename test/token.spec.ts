dfbbbb83da65f0d3f2a658a1695d410600798b7a
ruann3res <ruanpablo1218@gmail.com>
2025-04-19 20:16:31 -0300

    test: test for sign userOrquestrator

--- test/token.spec.ts
+++ test/token.spec.ts
@@ -1,4 +1,4 @@
-import Token, { UserTokenPayload } from "../src/domain/model/token";
+import Token, { OrquestratorTokenPayload, UserTokenPayload } from "../src/domain/model/token";
 import { afterAll, beforeAll, describe, expect, it, jest } from "@jest/globals";
 import Environment from "../src/utils/environment";
 import jwt from "jsonwebtoken";
@@ -7,6 +7,8 @@ import { mockEnvironment } from "./mocks/environment";
 
 describe("Token Class", () => {
   const payload: UserTokenPayload = { userUuid: "123", email: "test@example.com" };
+  const payloadOrquestrator: OrquestratorTokenPayload = { orquestratorUuid: "123", userUuid: "123" };
+
   const secretKey = mockEnvironment.TOKEN_SECRET_KEY;
 
   beforeAll(() => {
@@ -39,6 +41,28 @@ describe("Token Class", () => {
     const invalidToken = "invalid.token.here";
     expect(() => {
       Token.verifyUser(invalidToken);
+    }).toThrow(ErrorCode.INVALID_ACCESS_TOKEN);
+  });
+
+  it("should sign a token correctly for orquestrator", () => {
+    const token = Token.signOrquestrator(payloadOrquestrator);
+    expect(typeof token).toBe("string");
+    const decoded = jwt.verify(token, secretKey) as OrquestratorTokenPayload;
+    expect(decoded.orquestratorUuid).toBe(payloadOrquestrator.orquestratorUuid);
+    expect(decoded.userUuid).toBe(payloadOrquestrator.userUuid);
+  });
+
+  it("should verify a token correctly for orquestrator", () => {
+    const token = Token.signOrquestrator(payloadOrquestrator);
+    const decoded = Token.verifyOrquestrator(token);
+    expect(decoded.orquestratorUuid).toBe(payloadOrquestrator.orquestratorUuid);
+    expect(decoded.userUuid).toBe(payloadOrquestrator.userUuid);
+  });
+
+  it("should throw an error for an invalid token for orquestrator", () => {
+    const invalidToken = "invalid.token.here";
+    expect(() => {
+      Token.verifyOrquestrator(invalidToken);
     }).toThrow(ErrorCode.INVALID_ACCESS_TOKEN);
   });
 });
